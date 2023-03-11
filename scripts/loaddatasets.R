6915418c50bb5e3f8f5e10313abbc5672e3274de
Tony Kabilan Okeke <tonykabilanokeke@gmail.com>
2023-03-11 05:25:37 +0000

    Remove biomart from foreach

--- scripts/loaddatasets.R
+++ scripts/loaddatasets.R
@@ -34,6 +34,8 @@ logs <- "./logs"
 
 # ----------------------------------- DATA ---------------------------------- #
 
+message("Downloading metadata and annotations...")
+
 # Download and filter the metadata 
 # -- For now, only use datasets with 1000 or fewer samples
 metadata <- getDEE2::getDEE2Metadata(species) %>%
@@ -45,17 +47,23 @@ metadata <- getDEE2::getDEE2Metadata(species) %>%
     ungroup()
 GEO_series <- unique(metadata$GEO_series)[1:5]
 
-# Retrieve a biomaRt
-ensembl <- biomaRt::useMart(
-    "ensembl", dataset = "mmusculus_gene_ensembl", host = "useast.ensembl.org"
-)
-
 # Count completed datasets
 N <- length(GEO_series)
 
+# Retrieve a biomaRt
+ensembl <- biomaRt::useMart(
+    "ensembl", dataset = "mmusculus_gene_ensembl", 
+    host = "useast.ensembl.org"
+)
+symbols <- biomaRt::getBM(
+    attributes = c("ensembl_gene_id", "external_gene_name"), mart = ensembl
+) %>%
+    rename(gene_id = ensembl_gene_id, symbol = external_gene_name) %>%
+    filter(!is.na(symbol))
+
 
 # ------------------------------ PARALLELIZATION ---------------------------- #
-
+GEO_series <- sample(unique(metadata$GEO_series), 5)
 CL <- makeCluster(2, outfile = paste0(logs, "/loaddatasets.log"))
 registerDoSNOW(CL)
 
@@ -78,119 +86,116 @@ res <- foreach(
     .combine = rbind, 
     .options.snow = opts,
     .packages = c("dplyr", "stringr", "tibble", "getDEE2", "GEOquery", "limma",
-                 "GOfuncR", "biomaRt")
+                 "GOfuncR")
 ) %dopar% {
 
     GSE <- GEO_series[i]
     message("Processing ", GSE, " (", i, "/", N, ")")
 
-    # Download the GSE metadata
-    gse_meta <- suppressMessages( GEOquery::getGEO(
-        GSE, destdir = temp, GSEMatrix = FALSE, getGPL = FALSE
-    ) )
-    gsms <- GEOquery::GSMList(gse_meta)
+    tryCatch({
+        # Download the GSE metadata
+        gse_meta <- suppressMessages( GEOquery::getGEO(
+            GSE, destdir = temp, GSEMatrix = FALSE, getGPL = FALSE
+        ) )
+        gsms <- GEOquery::GSMList(gse_meta)
 
-    # Retrieve sample sources and characteristics
-    characs <- as.data.frame( rbind(
-        sapply(gsms, function(x) x@header$source_name_ch1),
-        sapply(gsms, function(x) x@header$characteristics_ch1)
-    ) )
-    characs <- characs[
-        !grepl("librar[^:]*:", characs[,1]) & 
-        !grepl("read[^:]*:", characs[,1]),
-    ]
-    characs <- characs[
-        apply(characs, 1, function(x) length(unique(x)) > 1), 
-    ]
-
-    if (nrow(characs) == 0) {  # No characteristics
-        SKIP <- TRUE
-    } else if (nrow(characs) == 1) {  # Single characteristic
-        characs <- unlist(characs)
-        unique_characs <- unique(characs)
-        groups <- sapply(characs, function(x) which(unique_characs == x))
-        groups[groups != 1] <- "B"
-        groups[groups == 1] <- "A"
-        SKIP <- FALSE
-    } else {  # Multiple characteristics
-        characs <- unlist( characs[
-            which.min( apply(characs, 1, function(x) length(unique(x))) ), 
-        ] )
-        unique_characs <- unique(characs)
-        groups <- sapply(characs, function(x) which(unique_characs == x))
-        groups[groups != 1] <- "B"
-        groups[groups == 1] <- "A"
-        SKIP <- FALSE
-    }
-
-    if (SKIP) {
-        c(GSE, NA, NA)
-    } else {
-        # Get SRR accessions
-        SRRs <- metadata$SRR_accession[metadata$GEO_series == GSE]
-        groups <- groups[
-            names(groups) %in% metadata$GSM_accession[metadata$GEO_series == GSE]
+        # Retrieve sample sources and characteristics
+        characs <- as.data.frame( rbind(
+            sapply(gsms, function(x) x@header$source_name_ch1),
+            sapply(gsms, function(x) x@header$characteristics_ch1)
+        ) )
+        characs <- characs[
+            !grepl("librar[^:]*:", characs[,1]) & 
+            !grepl("read[^:]*:", characs[,1]),
+        ]
+        characs <- characs[
+            apply(characs, 1, function(x) length(unique(x)) > 1), 
         ]
 
-        # Split SRRs into chunks of 600
-        if (length(SRRs) > 600) {
-            SRR_list <- split(SRRs, ceiling(seq_along(SRRs) / 600))
-        } else {
-            SRR_list <- list(SRRs)
+        if (nrow(characs) == 0) {  # No characteristics
+            SKIP <- TRUE
+        } else if (nrow(characs) == 1) {  # Single characteristic
+            characs <- unlist(characs)
+            unique_characs <- unique(characs)
+            groups <- sapply(characs, function(x) which(unique_characs == x))
+            groups[groups != 1] <- "B"
+            groups[groups == 1] <- "A"
+            SKIP <- FALSE
+        } else {  # Multiple characteristics
+            characs <- unlist( characs[
+                which.min( apply(characs, 1, function(x) length(unique(x))) ), 
+            ] )
+            unique_characs <- unique(characs)
+            groups <- sapply(characs, function(x) which(unique_characs == x))
+            groups[groups != 1] <- "B"
+            groups[groups == 1] <- "A"
+            SKIP <- FALSE
         }
 
-        # Download counts
-        counts <- lapply(SRR_list, function(SRR) {
-            SE <- suppressMessages( suppressWarnings( getDEE2::getDEE2(
-                species, SRR, metadata = metadata, counts = "GeneCounts",
-                quiet = TRUE
-            ) ) )
-            assays(SE)$counts
-        }) %>%
-            do.call(cbind, .)
-
-        # Perform DE analysis
-        groups <- factor(groups)
-        design <- model.matrix(~0 + groups)
-        colnames(design) <- levels(groups)
-        contrast <- limma::makeContrasts(A - B, levels = design)
-        fit <- limma::lmFit(counts, design)
-        fit <- limma::contrasts.fit(fit, contrast)
-        fit <- limma::eBayes(fit)
-        deg <- limma::topTable(fit, number = Inf) %>%
-            tibble::rownames_to_column("gene_id") %>%
-            mutate(is_candidate = as.numeric(adj.P.Val < 0.05))
-
-        if ( !any(deg$is_candidate == 1) ) {
-            NA
+        if (SKIP) {
+            c(GSE, NA, NA)
         } else {
-            # Update gene symbols
-            symbols <- biomaRt::getBM(
-                attributes = c("ensembl_gene_id", "external_gene_name"),
-                filters = "ensembl_gene_id", values = deg$gene,
-                mart = ensembl
-            ) %>%
-                dplyr::rename(gene_id = ensembl_gene_id, symbol = external_gene_name) %>%
-                dplyr::filter(!is.na(symbol))
-            deg <- left_join(deg, symbols, by = "gene_id") %>%
-                tidyr::drop_na(symbol)
+            # Get SRR accessions
+            SRRs <- metadata$SRR_accession[metadata$GEO_series == GSE]
+            groups <- groups[
+                names(groups) %in% metadata$GSM_accession[metadata$GEO_series == GSE]
+            ]
 
-            # Gene Ontology enrichment
-            res <- suppressMessages( suppressWarnings( GOfuncR::go_enrich(
-                deg[c("symbol", "is_candidate")], organismDb = "org.Mm.eg.db",
-                n_randsets = 100, silent = TRUE
-            ) ) )$results %>%
-                filter(FWER_underrep < 0.05 | FWER_overrep < 0.05)
+            # Split SRRs into chunks of 600
+            if (length(SRRs) > 600) {
+                SRR_list <- split(SRRs, ceiling(seq_along(SRRs) / 600))
+            } else {
+                SRR_list <- list(SRRs)
+            }
 
-            # Delete downloaded files
-            unlink(paste0(temp, "/*"))
+            # Download counts
+            counts <- lapply(SRR_list, function(SRR) {
+                SE <- suppressMessages( suppressWarnings( getDEE2::getDEE2(
+                    species, SRR, metadata = metadata, counts = "GeneCounts",
+                    quiet = TRUE
+                ) ) )
+                assays(SE)$counts
+            }) %>%
+                do.call(cbind, .)
 
-            # Store results in row vector
-            row <- c(GSE, deg$logFC, paste(res$node_id, collapse = ";"))
-            names(row) <- c("GSE", deg$symbol, "GO_terms")
-            row
+            # Perform DE analysis
+            groups <- factor(groups)
+            design <- model.matrix(~0 + groups)
+            colnames(design) <- levels(groups)
+            contrast <- limma::makeContrasts(A - B, levels = design)
+            fit <- limma::lmFit(counts, design)
+            fit <- limma::contrasts.fit(fit, contrast)
+            fit <- limma::eBayes(fit)
+            deg <- limma::topTable(fit, number = Inf) %>%
+                tibble::rownames_to_column("gene_id") %>%
+                mutate(is_candidate = as.numeric(adj.P.Val < 0.05))
+
+            if ( !any(deg$is_candidate == 1) ) {
+                c(GSE, NA, NA)
+            } else {
+                # Update gene symbols
+                deg <- left_join(deg, symbols, by = "gene_id") %>%
+                    tidyr::drop_na(symbol)
+
+                # Gene Ontology enrichment
+                res <- suppressMessages( suppressWarnings( GOfuncR::go_enrich(
+                    deg[c("symbol", "is_candidate")], organismDb = "org.Mm.eg.db",
+                    n_randsets = 100, silent = TRUE
+                ) ) )$results %>%
+                    filter(FWER_underrep < 0.05 | FWER_overrep < 0.05)
+
+                # Delete downloaded files
+                unlink(paste0(temp, "/*"))
+
+                # Store results in row vector
+                row <- c(GSE, deg$logFC, paste(res$node_id, collapse = ";"))
+                names(row) <- c("GSE", deg$symbol, "GO_terms")
+                row
+            }
         }
-    }
+    }, error = function(e) {
+        c(GSE, NA, NA)
+    })
 }
 end_time <- Sys.time()
 message("\nElapsed time: ", end_time - start_time)
