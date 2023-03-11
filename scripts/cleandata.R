fc778f29441de544246fa34386f685828b240ee5
Tony Kabilan Okeke <tonykabilanokeke@gmail.com>
2023-03-11 00:19:55 +0000

    Add script for converting raw output to a clean data framew

---
+++ scripts/cleandata.R
@@ -0,0 +1,49 @@
+
+# ----------------------------------- SETUP --------------------------------- #
+
+# Set CRAN mirror
+options(repos = c(CRAN = "http://lib.stat.cmu.edu/R/CRAN/"))
+
+# Install packages
+cran <- c("dplyr", "stringr", "tibble", "tidyr", "yaml", "feather")
+cran <- cran[!(cran %in% installed.packages()[, "Package"])]
+if (length(cran)) install.packages(cran)
+
+# Load packages
+require(dplyr)
+require(stringr)
+require(tidyr)
+require(tibble)
+require(feather)
+
+# Configuration
+temp <- "./data/.temp"
+data <- "./data"
+logs <- "./logs"
+# config <- yaml::read_yaml("config/config.yaml")$scripts$loaddatasets
+
+# ------------------------------ DATA CLEANING ------------------------------ #
+
+# Load results
+res <- readRDS(paste0(data, "/processed/DEG_results.rds"))
+
+# Clean up matrix
+rownames(res) <- NULL
+missing <- res[!complete.cases(res),"GSE"]
+res <- as.data.frame(res[complete.cases(res),])
+
+# Add suffix to duplicate column names
+cols <- names(res)[-c(1, length(names(res)))]
+dups <- cols[duplicated(cols)]
+if ( length(dups) > 0 ) {
+    cols[duplicated(cols)] <- paste0(cols[duplicated(cols)], "_", seq_along(dups))
+}
+names(res) <- c("GSE", cols, "GO_terms")
+
+# Convert columns to numeric
+res[cols] <- lapply(res[cols], as.numeric)
+
+# Save to feather
+write_feather(res, paste0(data, "/processed/data-cleaned.feather"))
+
+message("Data cleaned.")
