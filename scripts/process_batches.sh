ddd0de69c62ce9b011b421ba5753e11745dfe989
GCloud <tony@gloud>
2023-03-13 07:33:41 +0000

    Add code to download metadata befor executing batches

--- scripts/process_batches.sh
+++ scripts/process_batches.sh
@@ -7,6 +7,8 @@
 # Once all 3 batches are complete, the `prepdata.py` script will be run to prepare
 # the feature matrix and target vector for the machine learning models.
 
+Rscript scripts/loadmetadata.R
+
 for file in data/raw/batch-*-GSE.txt; do
     batch=${file#data/raw/batch-}
     batch=${batch%-GSE.txt}
@@ -21,4 +23,6 @@ for file in data/raw/batch-*-GSE.txt; do
         echo "Error: Batch $batch failed"
         echo "data/processed/batch-${batch}-DEG.rds not found for batch $batch"
     fi
+
+    echo "\n\n"
 done
