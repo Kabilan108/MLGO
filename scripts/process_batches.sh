a77dea23a10bc9fd21c57e27cb77b82ff81eb775
Kabilan <kabilanokeke@gcloud>
2023-03-15 18:38:15 +0000

    Add commands to zip the results

--- scripts/process_batches.sh
+++ scripts/process_batches.sh
@@ -2,10 +2,14 @@
 #
 # This script will download over 11000 datasets from the DEE2 repository using the
 # `loaddatasets.R` script.
-# Datasets will be loaded in batches of ~3500 datasets. After each batch, the
+# Datasets will be loaded in batches of ~950 datasets. After each batch, the
 # `cleandata.R` script will be run to clean the data format the results as dataframes.
-# Once all 3 batches are complete, the `prepdata.py` script will be run to prepare
+# Once all batches are complete, the `prepdata.py` script will be run to prepare
 # the feature matrix and target vector for the machine learning models.
+# Finally, the `prepdata.py` script will be run to prepare the feature matrix
+# and target vector for the machine learning models. It will also perform the
+# train/validation/test split.
+# This script will take several hours to run.
 
 Rscript scripts/loadmetadata.R
 echo -e "\n\n"
@@ -29,3 +33,8 @@ for file in data/raw/batch-*.txt; do
 done
 
 python scripts/prepdata.py
+
+echo -e "\n\nZipping data files..."
+tar -czf data/final/data.tar.gz data/final/*.npz
+
+echo -e "\n\nDone!"
